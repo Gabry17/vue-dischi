96fb94300d4850088ce2fcb8f5174d307adad10f
Gabry17 <gbianchi528@gmail.com>
2022-05-20 15:54:18 +0200

    aggiunto stile al select

--- src/components/AppGenre.vue
+++ src/components/AppGenre.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="select">
-    <select name="" id="" v-model='searchGenre' @change="$emit('searchChange', searchGenre)">
-      <option value=""></option>
-      <option value="pop">Pop</option>
-      <option value="rock">Rock</option>
-      <option value="metal">Metal</option>
-      <option value="jazz">Jazz</option>
+    <label for="filterGenre">Generi:</label>
+    <select name="filter genre" id="filterGenre" v-model='searchGenre' @change="$emit('searchChange', searchGenre)">
+        <option value="">All</option>
+        <option value="pop">Pop</option>
+        <option value="rock">Rock</option>
+        <option value="metal">Metal</option>
+        <option value="jazz">Jazz</option>
     </select>
   </div>
 </template>
@@ -22,5 +23,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.select{
+    margin-bottom: 1em;
+    label{
+    color: white;
+    margin-right: .5em;
+    }
+}
 </style>
